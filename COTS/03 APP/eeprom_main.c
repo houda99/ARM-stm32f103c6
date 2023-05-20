71483af2891341803e3213c260c23fe375abb973
houda99 <42821357+houda99@users.noreply.github.com>
2023-05-20 03:11:53 +0300

    update to eeprom_main.c

--- COTS/03 APP/eeprom_main.c
+++ COTS/03 APP/eeprom_main.c
@@ -16,26 +16,26 @@ int main(){
 	LED_init(PORTB, 0);
 	I2C_Config();
 	I2C_Start();
-	I2C_Address(0b10100100);
-	_delay_ms(100);
-	I2C_Write(0X00);
-	_delay_ms(100);
-	I2C_Write(0x09);
-	I2C_Stop();
-	_delay_ms(1000);
-	I2C_Start();
-	I2C_Address(0b10100101);
-	_delay_ms(1000);
-	I2C_Read(0b10100101, &read_data, 1);
+	// I2C_Address(0b10100100);
+	// _delay_ms(100);
+	// I2C_Write(0X00);
+	// _delay_ms(100);
+	// I2C_Write(0x09);
+	// I2C_Stop();
+	// _delay_ms(1000);
+	// I2C_Start();
+	// I2C_Address(0b10100101);
+	// _delay_ms(1000);
+	// I2C_Read(0b10100101, &read_data, 1);
 
 	while(1){
-//		EEPROM_Write(100, 0x09);
-//		LED_on(PORTB, 0);
-//		_delay_ms(1000);
-//		LED_off(PORTB, 0);
-//		if(EEPROM_Read(100)==0x09){
-//			LED_on(PORTB, 0);
-//		}
+		EEPROM_Write(100, 0x09);
+		LED_on(PORTB, 0);
+		_delay_ms(1000);
+		LED_off(PORTB, 0);
+		if(EEPROM_Read(100)==0x09){
+		LED_on(PORTB, 0);
+		}
 
 	}
 	return 0;
