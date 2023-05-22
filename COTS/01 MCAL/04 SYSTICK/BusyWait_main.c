b095b6b399786f90e3c78b6f6b231b9116dc22b9
houda99 <42821357+houda99@users.noreply.github.com>
2023-05-22 23:26:03 +0300

    MODIFY SYSTICK

--- COTS/01 MCAL/04 SYSTICK/BusyWait_main.c
+++ COTS/01 MCAL/04 SYSTICK/BusyWait_main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include	"Std_Types.h"
 #include	"Bit_Math.h"
 
@@ -34,40 +33,3 @@ int main()
 
 
 
-=======
-#include	"Std_Types.h"
-#include	"Bit_Math.h"
-
-#include	"RCC_interface.h"
-#include	"DIO_interface.h"
-#include	"STK_interface.h"
-
-
-/*	C13 ==> Built in led (Toggle Led)		*/
-int main()
-{
-	/*	System Clock Init			*/
-	RCC_voidSysClkInit();
-	/*	Enable RCC for GPIOC 		*/
-	RCC_voidEnablePerClk(RCC_APB2,4);
-	
-	/*	C13 Direction 	"Output @ 2MHz Push Pull"	*/
-	DIO_voidSetPinDirection(PORTC,PIN13,GPIO_OUTPUT_2MHZ_PP);
-	
-	/*	SysTick	Init				*/
-	STK_voidInit();
-	while(1)
-	{
-		/*		Led On		*/
-		DIO_voidSetPinValue(PORTC,PIN13,GPIO_LOW);
-		STK_voidSetBusyWait(1000000);
-		/*		Led Off		*/
-		DIO_voidSetPinValue(PORTC,PIN13,GPIO_HIGH);		
-		STK_voidSetBusyWait(1000000);
-	}
-	return 0;
-}
-
-
-
->>>>>>> 81f24b8ec317b796073e6525e6dd9a2900465ae9
--- COTS/01 MCAL/04 SYSTICK/STK_interface.h
+++ COTS/01 MCAL/04 SYSTICK/STK_interface.h
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-
-
-
-
-
+#ifndef STK_INTEFACE_
+#define STK_INTEFACE_
 void	STK_voidInit();
 void	STK_voidSetBusyWait(u32	Ticks);
 void	STK_voidSetIntervalSingle(u32	Ticks, void (*Pf)(void));
 void	STK_voidSetIntervalPeriodic(u32	Ticks, void (*Pf)(void));
 void	STK_voidStopInterval();
 u32		STK_u32GetElapsedTime();
-=======
-
-
-
-
-
-void	STK_voidInit();
-void	STK_voidSetBusyWait(u32	Ticks);
-void	STK_voidSetIntervalSingle(u32	Ticks, void (*Pf)(void));
-void	STK_voidSetIntervalPeriodic(u32	Ticks, void (*Pf)(void));
-void	STK_voidStopInterval();
-u32		STK_u32GetElapsedTime();
->>>>>>> 81f24b8ec317b796073e6525e6dd9a2900465ae9
 u32		STK_u32GetRemainingTime();
+#endif
